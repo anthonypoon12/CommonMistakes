103aabe44ae737e12c67be6f6bdb1f1c230764c0
anthonypoon12 <anthony@poons.us>
2022-07-24 15:47:34 -0400

    Declared variables to fix problem

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
+"use strict"
 let specialnumber = Math.floor(Math.random()*18);
-let sentence = sentences[specialnumber/2];
-let sentencePart1=sentence.part1;
-let sentencePart2=sentence.part2;
+let sentence = sentences[Math.floor(specialnumber/2)];
+let sentencePart1=sentence.partOne;
+let sentencePart2=sentence.partTwo;
+console.log(sentencePart1);
+console.log(sentencePart2);
 let specialword="";
 if (specialnumber%2==0)
     specialword=sentence.wrong;
 else
     specialword=sentence.right;
 function createProblemDiv(sentencePart1, sentencePart2){
-    problemDiv = document.createElement('div');
+    let problemDiv = document.createElement('div');
     problemDiv.id = ('problem');
     problemDiv.classList.add('english');
     problemDiv.classList.add('border-bottom');
@@ -18,10 +21,10 @@ function createProblemDiv(sentencePart1, sentencePart2){
     return problemDiv;
 }
 function createTargetWordSpan(specialword){
-    targetWordSpan = document.createElement('span');
+    let targetWordSpan = document.createElement('span');
     targetWordSpan.id = 'specialword';
     targetWordSpan.innerHTML = specialword;
     return targetWordSpan;
 }
-problemContainer = document.getElementById("problemContainer");
+let problemContainer = document.getElementById("problemContainer");
 $('#problemContainer').prepend(createProblemDiv(sentencePart1, sentencePart2));
