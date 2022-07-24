c2f04f884dc9715dad14874b7f1c8c8f13744b11
anthonypoon12 <anthony@poons.us>
2022-07-24 15:47:02 -0400

    Added meanings to sentences dictionary

--- sentences.js
+++ sentences.js
@@ -3,54 +3,63 @@ sentences = {
         partOne: "美國",
         wrong: "是",
         right: "很",
-        partTwo:"大"
+        partTwo:"大。",
+        meaning:"The United States is very big."
     },
     1: {
         partOne: "我",
         wrong: "是",
         right: "上",
-        partTwo:"大學二年級"
+        partTwo:"大學二年級。",
+        meaning:"I am a sophomore in college."
     },
     2: {
         partOne: "你有幾",
         wrong: "個",
         right: "隻",
-        partTwo:"狗"
+        partTwo:"狗。",
+        meaning: "You have a few dogs."
     },
     3: {
         partOne: "你有",
         wrong: "不",
         right: "沒",
-        partTwo:"有中國朋友"
+        partTwo:"有中國朋友。",
+        meaning: "Do you have Chinese friends?"
     },
     4: {
         partOne: "學校",
         wrong: "是",
         right: "在",
-        partTwo:"我們的宿舍旁邊"
+        partTwo:"我們的宿舍旁邊。",
+        meaning:"The school is next to our dormitory."
     },
     5: {
         partOne: "我的高中有",
         wrong: "十五千",
         right: "一萬五千",
-        partTwo:"個學生"
+        partTwo:"個學生。",
+        meaning: "My high chool has 15,000 students."
     },
     6: {
         partOne: "中國菜",
         wrong: "還是",
         right: "或是",
-        partTwo:"美國才都很好吃"
+        partTwo:"美國才都很好吃。",
+        meaning: "Chinese food and American food are both very good."
     },
     7: {
         partOne: "我喜歡吃中國飯",
         wrong: "和",
         right: "也",
-        partTwo:"喜歡學中文"
+        partTwo:"喜歡學中文。",
+        meaning:"I like to eat Chinese food and I like to learn Chinese."
     },
     8: {
         partOne: "我不",
         wrong: "可以",
         right: "會",
-        partTwo:"説中文"
+        partTwo:"説中文。",
+        meaning: "I can't speak Chinese."
     },
 };
