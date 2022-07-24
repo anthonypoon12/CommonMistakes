b9b91cd224d8d9ef8ebd8afe2e5d3daa29d0401e
anthonypoon12 <anthony@poons.us>
2022-07-24 18:08:33 -0400

    made loadsentence its own function
    fixed true/false buttons

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
 "use strict"
+var correct = 0;
+var trueOrFalse = false;
+//Makes array of indexes for sentences
 const setOfSentences = [];
+var problemContainer = document.getElementById("problemContainer");
 for (let i = 0 ; i < 18; i++){
     setOfSentences.push(i);
 }
-let specialnumber = Math.floor(Math.random()*18);
-let indexToRemoveFromSet;
-indexToRemoveFromSet = setOfSentences.indexOf(specialnumber);
-if(specialnumber%2==1)
-indexToRemoveFromSet--;
-if (indexToRemoveFromSet !== -1) {
-    setOfSentences.splice(indexToRemoveFromSet, 2);
+//loads the first sentence
+window.addEventListener('load', loadSentence);
+function loadSentence(){ 
+    let index = Math.floor(Math.random()*setOfSentences.length);
+    let specialnumber = setOfSentences[index];
+    let indexToRemoveFromSet=index;
+    if(index%2==1)
+        indexToRemoveFromSet--;
+    if (indexToRemoveFromSet !== -1) {
+        setOfSentences.splice(indexToRemoveFromSet, 2);
+    }
+    let sentence = sentences[Math.floor(specialnumber/2)];
+    let sentencePart1=sentence.partOne;
+    let sentencePart2=sentence.partTwo;
+    let meaning=sentence.meaning;
+    let specialword="";
+    if (specialnumber%2==0){
+        specialword=sentence.wrong;
+        trueOrFalse = false;
+    }
+    else{
+        specialword=sentence.right;
+        trueOrFalse = true;
+    }
+    $('#problemContainer').prepend(createProblemDiv(sentencePart1, sentencePart2, specialword, meaning));
 }
-let sentence = sentences[Math.floor(specialnumber/2)];
-let sentencePart1=sentence.partOne;
-let sentencePart2=sentence.partTwo;
-let meaning=sentence.meaning;
-console.log(sentencePart1);
-console.log(sentencePart2);
-let specialword="";
-if (specialnumber%2==0)
-    specialword=sentence.wrong;
-else
-    specialword=sentence.right;
-function createProblemDiv(sentencePart1, sentencePart2){
+function createProblemDiv(sentencePart1, sentencePart2, specialword, meaning){
     let problemDiv = document.createElement('div');
     problemDiv.id = ('problem');
     problemDiv.classList.add('english');
@@ -46,5 +57,9 @@ function createMeaningSpan(meaning){
     meaningSpan.innerHTML = meaning;
     return meaningSpan;
 }
-let problemContainer = document.getElementById("problemContainer");
-$('#problemContainer').prepend(createProblemDiv(sentencePart1, sentencePart2));
+$( "#correct" ).click(function() {
+    alert(true===trueOrFalse);
+});
+$( "#incorrect" ).click(function() {
+    alert(false===trueOrFalse);
+});
