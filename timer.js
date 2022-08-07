dd976672ea57dee288294182d4f5e938c17aee84
anthonypoon12 <anthony@poons.us>
2022-08-07 02:03:51 -0400

    fix timer js

--- timer.js
+++ timer.js
@@ -1,3 +1,4 @@
+var timer;
 $(document).ready(function () {
 
     var second = 0;
@@ -5,12 +6,12 @@ $(document).ready(function () {
     function convert(val) {
       return val > 9 ? val : "0" + val;
     }
-    var timer = setInterval(function () {
+    timer = setInterval(function () {
       $("#seconds").text(convert(++second % 60));
       $("#minutes").text(convert(parseInt(second / 60, 10)));
     }, 1000);
   
-    function clearTimer() {
-      clearInterval(timer);
-    }
-})
+  })
+  function clearTimer() {
+    clearInterval(timer);
+  }
