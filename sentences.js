c1e3cbc2f48a0a0602dd85d7b113480e81647d05
anthonypoon12 <anthony@poons.us>
2022-10-02 19:46:34 -0400

    Added everything to simplified

--- sentences.js
+++ sentences.js
@@ -512,10 +512,15 @@ sentences = {
 				right: "我家离学校很近。",
 				meaning: "My home is very close to the school.",
 			},
-			16:{
-				wrong: "那家餐厅有点干净。",
-				right: "那家餐厅有点不干净。",
-				meaning: "That restuarant is a little dirty.",
+			"Notes":{
+				0:["Adjectives function as verbs in Chinese. Therefore, 是 cannot be used before adjectives. However, the word 很, a minimum modifier should be put before the adjective.",[0,1,2,3,4,5,6]],
+				1:["A minimum modifier 很 is missing. 多 alone cannot be put before an adjective followed by a noun. One has to use 很多.",[7,8]],
+				2:["In the 比 construction, 很 cannot be used before the adjective.",[9]],
+				3:["很 cannot be used before adj + 极了. 很 cannot be used with the reduplicate form of an adjective such as 干干净净. The correct form is: 很干净.",[10,11]],
+				4:["The word ‘energetic’ is 有精神. 很 can be added in front of 有精神. For example, 兴趣 (interest), 有兴趣 (interested), 很有兴趣 (very interested).",[12]],
+				5:["In this construction, ‘大' or words in positive connotation should appear first. For example, 不高不矮, 不多不少, 不胖不瘦, 不冷不热, 不快不慢.",[13]],
+				6:["The negation marker 不 should go before the verb 是. For example, 他是好学生, 他不是好学生. It is OK to say 好学生 or 坏学生. The expression 不好学生 is wrong. ",[14]],
+				7:["The word to express distance 近 or 远 should be used with 离.",[15]],
 			},
 		},
 		"Errors in Measure Words": {
@@ -531,142 +536,138 @@ sentences = {
 				meaning: "Is your dog a big dog or a small dog?",
 			},
 			2:{
-				wrong: "我家有两个猫。一是大，另一是小。",
-				right: "我家有两只猫。一只大，另只是小。",
-				meaning: "I have two cats in my house. One is big, the other is small.",
+				wrong: "我家有两个猫。",
+				right: "我家有两只猫。",
+				meaning: "I have two cats in my house.",
 			},
 			3:{
+				wrong: "一是大，另一是小。",
+				right: "一只大，另只是小。",
+				meaning: "One(cat) is big, the other is small.",
+			},
+			4:{
 				wrong: "我有三个中文书",
 				right: "我有三本中文书",
 				meaning: "I have three Chinese books.",
 			},
-			4:{
+			5:{
 				wrong: "你有多少钱？我有二十钱。",
 				right: "你有多少钱？我有二十块钱。",
 				meaning: "How much money do you have? I have twenty dollars.",
 			},
-			5:{
+			6:{
 				wrong: "我有三中国朋友。",
 				right: "我有三个中国朋友。",
 				meaning: "I have three Chinese friends.",
 			},
-			6:{
+			7:{
 				wrong: "我在高中学了三个年中文。",
 				right: "我在高中学了三年中文。",
 				meaning: "I learned three years of Chinese in high school.",
 			},
-			7:{
+			8:{
 				wrong: "我一个星期上中文课一次。",
 				right: "我一个星期上一次中文课。",
 				meaning: "I attend Chinese class once a week.",
 			},
-			8:{
+			9:{
 				wrong: "他一个月一次吃中国饭。",
 				right: "他一个月吃一次中国饭。",
 				meaning: "He eats Chinese food once a month.",
 			},
-			9:{
+			10:{
 				wrong: "今天比昨天一点冷。",
 				right: "今天比昨天冷一点。",
 				meaning: "Today is a little colder than yesterday.",
 			},
-			10:{
+			11:{
 				wrong: "昨天冷得要命，今天一点儿暖和了。",
 				right: "昨天冷得要命，今天暖和一点儿了。",
 				meaning: "Yesterday was terribly cold, today it's a little warmer.",
 			},
-			11:{
+			12:{
 				wrong: "这家餐厅比那家餐厅一点贵。",
 				right: "这家餐厅比那家餐厅贵一点。",
 				meaning: "This restaurant is a bit more expensive than that restaurant.",
 			},
-			12:{
+			13:{
 				wrong: "昨天下了雨一点儿。",
 				right: "昨天下了一点儿雨。",
 				meaning: "It rained a little yesterday.",
 			},
-			13:{
+			14:{
 				wrong: "我会说中文一点儿。",
 				right: "我会说一点儿中文。",
 				meaning: "I can speak a little Chinese.",
 			},
-			14:{
-				wrong: "这件衣服一点贵。",
-				right: "这件衣服有点贵。",
-				meaning: "This dress is a bit expensive.",
-			},
 			15:{
-				wrong: "我的朋友一点儿不舒服。",
-				right: "我的朋友有一点儿不舒服。",
-				meaning: "My friend is a little uncomfortable.",
-			},
-			16:{
-				wrong: "中文教室一点不干净。",
-				right: "中文教室一点也不干净。",
-				meaning: "The Chinese classroom is not clean at all.",
-			},
-			17:{
 				wrong: "他拿了有些蛋糕。",
 				right: "他拿了一些蛋糕。",
 				meaning: "He took some cakes.",
 			},
+			"Notes":{
+				0:["The measure word for animals is 隻.",[0,1,2,3]],
+				1:["Measure words such as 隻 or 個 must be used with a number or a determiner, as in 一隻狗, 這隻狗. 'Your dog' in Chinese is: 你的狗 not 你隻狗",[0,1,2,3,4,5,6,7,8,9]],
+				2:["After the number 一, the measure word should be used.",[3,4,5,6,7,8,9]],
+				3:["The measure word for books is: 本",[4]],
+				4:["After the number 二十, a measure word should be used.",[5]],
+				5:["After the number 三, a measure word should be used.",[6]],
+				6:["年 cannot be used with 個.",[7]],
+				7:["Frequency expressions should be put between the verb and the object.",[8,9]],
+				8:["For 比 construction,  the expressions ‘a bit’ or ‘more’ should go after the adjective.",[10,11,12]],
+				9:["Expressions ‘a bit’ should appear between the verb and the object. ",[13,14]],
+				10:["一些 meaning ‘some’ should be used before a noun. 有些 can only be used with undesirable adjectives, such as 蛋糕有些貴,",[15]],
+			},
 		},
 		"Errors in Conjunctions": {
 			0:{
-				wrong: "虽然我喜欢他，他不喜欢我。",
-				right: "虽然我喜欢他，但是他不喜欢我。",
+				wrong: "雖然我喜歡他，他不喜歡我。",
+				right: "雖然我喜歡他，但是他不喜歡我。",
 				meaning: "Although I like him, he doesn't like me.",
 			},
 			1:{
-				wrong: "虽然你不喜欢我，我还喜欢你。",
-				right: "虽然你不喜欢我，但是我还是喜欢你。",
+				wrong: "雖然你不喜歡我，我還喜歡你。",
+				right: "雖然你不喜歡我，但是我還是喜歡你。",
 				meaning: "Although you don't like me, I still like you.",
 			},
 			2:{
-				wrong: "虽然他的名字是王新，他不是我的新朋友。",
-				right: "虽然他的名字是王新，但是他不是我的新朋友。",
+				wrong: "雖然他的名字是王新，他不是我的新朋友。",
+				right: "雖然他的名字是王新，但是他不是我的新朋友。",
 				meaning: "Although his name is Wang Xin, he is not my new friend.",
 			},
 			3:{
-				wrong: "因为他是大学生，他的朋友也是大学生。",
-				right: "因为他是大学生，所以他的朋友也是大学生。",
+				wrong: "因為他是大學生，他的朋友也是大學生。",
+				right: "因為他是大學生，所以他的朋友也是大學生。",
 				meaning: "Because he is a college student, his friends are also college students.",
 			},
-			4:{
-				wrong: "他的屋子又整齐又干净。我不知道如果他住在那儿。",
-				right: "他的屋子又整齐又干净。我不知道他是不是住在那儿。",
-				meaning: "His house was neat and clean. I don't know if he lives there.",
-			},
-			5:{
-				wrong: "他要我问你如果你去体育馆跟他。",
-				right: "他要我问你跟不跟他去体育馆。",
-				meaning: "He wants me to ask if you follow him to the gym.",
+			"Notes":{
+				0:["雖然 and  但是 are used together to mean ‘although’",[0,1,2,3]],
 			},
 		},
 		"Errors in Verbs": {
 			0:{
-				wrong: "虽然我是学生，但是我喜欢教。",
-				right: "虽然我是学生，但是我喜欢教书。",
+				wrong: "雖然我是學生，但是我喜歡教。",
+				right: "雖然我是學生，但是我喜歡教書。",
 				meaning: "Although I am a student, I like to teach.",
 			},
 			1:{
-				wrong: "我是老师。我教中文。我喜欢教。",
-				right: "我是老师。我教中文。我喜欢教书。",
+				wrong: "我是老師。我教中文。我喜歡教。",
+				right: "我是老師。我教中文。我喜歡教書。",
 				meaning: "I'm a teacher. I teach Chinese. I like to teach.",
 			},
 			2:{
-				wrong: "他的办公室在我的旁边。我常常跟他说。",
-				right: "他的办公室在我的旁边。我常常跟他说话。",
+				wrong: "他的辦公室在我的旁邊。我常常跟他說。",
+				right: "他的辦公室在我的旁邊。我常常跟他說話。",
 				meaning: "His office is next to mine. I often talk to him.",
 			},
 			3:{
-				wrong: "他在家吗？我可以跟他说吗？",
-				right: "他在家吗？我可以跟他说话吗？",
+				wrong: "他在家嗎？我可以跟他說嗎？",
+				right: "他在家嗎？我可以跟他說話嗎？",
 				meaning: "Is he at home? can i talk to him?",
 			},
 			4:{
-				wrong: "我喜欢吃饭中国。",
-				right: "我喜欢吃中国饭。",
+				wrong: "我喜歡吃飯中國。",
+				right: "我喜歡吃中國飯。",
 				meaning: "I like to eat Chinese food.",
 			},
 			5:{
@@ -675,73 +676,96 @@ sentences = {
 				meaning: "There is a lot of snow this year.",
 			},
 			6:{
-				wrong: "我每天下午跑步一小时。",
-				right: "我每天下午跑一小时步。",
+				wrong: "我每天下午跑步一小時。",
+				right: "我每天下午跑一小時步。",
 				meaning: "I run for an hour every afternoon.",
 			},
 			7:{
-				wrong: "我昨天睡觉八个钟头。",
-				right: "我昨天睡八个钟头觉。",
-				meaning: "I slept eight hours yesterday.",
+				wrong: "我昨天睡覺八個鐘頭。",
+				right: "我昨天睡八個鐘頭覺。",
+				meaning: "Yesterday I slept for 8 hours.",
 			},
-			7:{
-				wrong: "我的姓是王。",
-				right: "我姓王。",
-				meaning: "My surname is Wang.",
+			"Notes":{
+				0:["‘To teach’ as a profession and ‘to talk’ as a general activity require the verb-object form. ",[0,1,2,3]],
+				1:["The modifier in a verb-object construction should be put in between the verb and object, not after the object.  ",[4,5,6,7]],
 			},
 		},
 		"Errors in Adverbs": {
 			0:{
-				wrong: "都我的学生喜欢学中文。",
-				right: "都我的学生喜欢学中文。",
+				wrong: "都我的學生喜歡學中文。",
+				right: "都我的學生喜歡學中文。",
 				meaning: "All my students like to learn Chinese.",
 			},
 			1:{
 				wrong: "他姓王。他也的朋友姓王。",
-				right: "他姓王。他也的朋友姓王。",
+				right: "他姓王。他的朋友也姓王。",
 				meaning: "His surname is Wang. His friend's surname is Wang.",
 			},
 			2:{
-				wrong: "他的爱人在学校也做事。",
-				right: "他的爱人在学校也做事。",
+				wrong: "他的愛人在學校也做事。",
+				right: "他的愛人在學校也做事。",
 				meaning: "His lover also works at school.",
 			},
 			3:{
-				wrong: "我一叫他请我吃饭，就他说没钱。",
-				right: "我一叫他请我吃饭，就他说没钱。",
+				wrong: "我一叫他請我吃飯，就他說沒錢。",
+				right: "我一叫他請我吃飯，就他說沒錢。",
 				meaning: "When I asked him to treat me to dinner, he said he had no money",
+			},
+			4:{
+				wrong: "那家餐廳有點乾淨。",
+				right: "那家餐廳有點不乾淨。",
+				meaning: "That restaurant is a bit unclean.",
+			},
+			5:{
+				wrong: "這件衣服一點貴。",
+				right: "這件衣服有點貴。",
+				meaning: "This dress is a bit expensive.",
+			},
+			6:{
+				wrong: "我的朋友一點兒不舒服。",
+				right: "我的朋友有一點兒不舒服。",
+				meaning: "My friend is a little uncomfortable.",
+			},
+			7:{
+				wrong: "中文教室一點不乾淨。",
+				right: "中文教室一點也不乾淨。",
+				meaning: "The Chinese classroom is not clean at all.",
+			},
+			"Notes":{
+				0:["都 and 就 are adverbs. They should be put before the verb and after the subject.",[0,1,2,3]],
+				1:["有點 can only be used with adjectives that are undesirable. Therefore, 不 must be used. You can also say: 那家餐廳算(suan4, can be considered) 乾淨 or 那家餐廳有點髒.",[4]],
+				2:["一點 cannot be used before an adjective. ",[5,6,7]],
 			},
 		},
 		"Errors in Nouns (Time, Place, Localizer)": {
 			0:{
-				wrong: "我工作在大学。",
-				right: "我在大学工作。",
+				wrong: "我工作在大學。",
+				right: "我在大學工作。",
 				meaning: "I work at university.",
 			},
 			1:{
-				wrong: "我上学在纽约。",
-				right: "我在纽约上学。",
+				wrong: "我上學在紐約。",
+				right: "我在紐約上學。",
 				meaning: "I go to school in New York.",
 			},
 			2:{
-				wrong: "你常常说中文在家跟父母吗？",
-				right: "你常常在家跟父母吗说中文？",
+				wrong: "你常常說中文在家跟父母嗎？",
+				right: "你常常在家跟父母嗎說中文？",
 				meaning: "Do you often speak Chinese with your parents at home?",
 			},
 			3:{
-				wrong: "他也教法文在这个学校。",
-				right: "他也在这个学校教法文。",
+				wrong: "他也教法文在這個學校。",
+				right: "他也在這個學校教法文。",
 				meaning: "He also teaches French at this school.",
 			},
 			4:{
-				wrong: "学校是我们的宿舍旁边。",
-				right: "学校旁边是我们的宿舍。",
-				meaning: "Next to the school is our dormitory.",
-			},
-			5:{
-				wrong: "他在哪儿？他在里家。",
-				right: "他在哪儿？他在里家。",
+				wrong: "他在哪兒？他在裏家。",
+				right: "他在哪兒？他在裏家。",
 				meaning: "Where is he? He is at home.",
+			},
+			"Notes":{
+				0:["Word order: place word before verb.",[0,1,2,3]],
+				1:["Word order: localizer goes after the noun.",[4]],
 			},
 		},
 		"Errors in Prepositions": {
@@ -755,10 +779,8 @@ sentences = {
 				right: "我们怎么从这儿去加拿大？",
 				meaning: "How do we get to Canada from here?",
 			},
-			2:{
-				wrong: "除了英文，以外我也会说中文。",
-				right: "除了英文以外，我也会说中文。",
-				meaning: "Besides English, I can also speak Chinese.",
+			"Notes":{
+				0:["Word order: a prepositional phrase should go before the verb.",[0,1]],
 			},
 		},
 		"Errors in Particles": {
@@ -773,22 +795,22 @@ sentences = {
 				meaning: "I can't eat what I like.",
 			},
 			2:{
-				wrong: "他说中文说的很好。",
-				right: "他说中文说得很好。",
-				meaning: "He speaks Chinese very well.",
-			},
-			3:{
 				wrong: "我从来没学义大利文。我不会说义大利文。",
 				right: "我从来没学过义大利文。我不会说义大利文。",
 				meaning: "I have never learned Italian. I don't speak Italian.",
 			},
-			4:{
+			3:{
 				wrong: "他以前很喜欢了她，可是现在不喜欢了。",
 				right: "他以前很喜欢她，可是现在不喜欢了。",
 				meaning: "He used to like her very much, but now he doesn't.",
 			},
+			"Notes":{
+				0:["In Chinese, modifier precedes modified (i.e. the noun).  的 should be put before the noun.",[0,1]],
+				1:["过 should be used after 从来没.",[2]],
+				2:["喜欢 is not an action verb. 了 should not be used even though it refers to the past.",[3]],
+			},
 		},
-		"Errors in Pronouns (Question Words,Demonstrative Pronoun)": {
+		"Errors in Pronouns (Question Words, Demonstrative Pronoun)": {
 			0:{
 				wrong: "他喜欢吃什么都。",
 				right: "他什么都喜欢吃。",
@@ -803,6 +825,25 @@ sentences = {
 				wrong: "他在这。",
 				right: "他在这里。",
 				meaning: "He is here.",
+			},
+			"Notes":{
+				0:["‘All’ is expressed by a question word. The word order is: S + 什么都 + verb. Similarly, S + 哪儿都 + verb.",[0,1]],
+				1:["It is better to say: 这里 unless there is a retroflex ending 儿. When you write, you have to write out both characters: 这儿.",[2]],
+			},
+		},
+		"Errors in 'if'": {
+			0:{
+				wrong: "他的屋子又整齐又干净。我不知道如果他住在那儿。",
+				right: "他的屋子又整齐又干净。我不知道他是不是住在那儿。",
+				meaning: "His house was neat and clean. I don't know if he lives there.",
+			},
+			1:{
+				wrong: "他要我问你如果你去体育馆跟他。",
+				right: "他要我问你跟不跟他去体育馆。",
+				meaning: "He wants me to ask if you want to follow him to the gym.",
+			},
+			"Notes":{
+				0:["It is ungrammatical to put 如果 after S + 不知道, A question construction should be used after 不知道.",[0,1]],
 			},
 		},
 	},
