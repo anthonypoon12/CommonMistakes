ca5c0a5101027f5e8c4484648904929ef84f3dfe
anthonypoon12 <anthony@poons.us>
2022-10-02 17:12:19 -0400

    added correct or incorrect to feedback

--- feedback.js
+++ feedback.js
@@ -3,8 +3,18 @@ const params = new URLSearchParams(window.location.search);
 const DICTIONARY = params.get('dict');
 var listOfResponses = localStorage.getItem(DICTIONARY).split(",");
 for (let i = 0; i < listOfResponses.length;i=i+2){
-    let rightorwrong = listOfResponses[i]%2==0? "wrong" : "right";
-    $("#correctbody").append(sentences[$(".btn-convert").text()][DICTIONARY][Math.floor(listOfResponses[i]/2)][rightorwrong]);
+    $("#correctbody").append(createGrid(i));
     $("#correctbody").append("<br>");
-    console.log("hey")
+}
+function createGrid(i){//number that represents sentence (right and wrong are different numbers)
+    let rightorwrong = listOfResponses[i]%2==0? "wrong" : "right";
+    let grid = `<div class="row">
+    <div class="col-sm">
+      ${sentences[$(".btn-convert").text()][DICTIONARY][Math.floor(listOfResponses[i]/2)][rightorwrong]}
+    </div>
+    <div class="col-sm">
+      ${listOfResponses[i+1]}
+    </div>
+  </div>`
+  return grid;
 }
