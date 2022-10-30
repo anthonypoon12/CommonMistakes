832a48707aa73b11370ea67297f242aa69dcbd6a
anthonypoon12 <anthony@poons.us>
2022-10-30 14:18:32 -0400

    added notes and correct message

--- feedback.js
+++ feedback.js
@@ -10,22 +10,30 @@ populateBodies();
 
 function populateBodies(){
     for (let i = 0; i < listOfResponses.length;i=i+2){
-        let rightorwrong = listOfResponses[i]%2==0? "wrong" : "right";
+        let rightorwrong = listOfResponses[i]%2==0? "wrong" : "right";// is the sentence right or wrong
         let choice;
         if (listOfResponses[i+1]=="N/A")
             choice = "N/A";
         else
             choice = listOfResponses[i+1]=="false" ? "incorrect" : "correct";
+        let notes = "";
+        if (choice=="N/A" || (rightorwrong=="wrong" && choice=="correct") || (rightorwrong=="right" && choice=="incorrect"))
+            notes = getNotes(listOfResponses[i]);
+        let userRightorWrong = "";
+        if (choice!="N/A")
+            userRightorWrong = `<br>You got it ${(rightorwrong=="wrong" && choice=="correct") || (rightorwrong=="right" && choice=="incorrect")? "wrong. 😔" : "right! 😎"}`;
         $("#accordion").append(
             `<div class="accordion-item">
             <h2 class="accordion-header" id="heading${i}">
               <button class="accordion-button fs-2 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${i}" aria-expanded="true" aria-controls="collapse${i}">
-              <span id="sentence${i}">${sentences[localStorage.getItem("Simplified")=="false" ? "Traditional" : "Simplified"][DICTIONARY][Math.floor(listOfResponses[i]/2)][rightorwrong]}</span>
+              <span id="sentence${i}">${sentences[simpOrTrad()][DICTIONARY][Math.floor(listOfResponses[i]/2)][rightorwrong]}</span>
               </button>
             </h2>
             <div id="collapse${i}" class="accordion-collapse collapse" aria-labelledby="heading${i}" data-bs-parent="#accordion">
               <div class="accordion-body fs-5">
                 You chose ${listOfResponses[i+1]}.
+                ${userRightorWrong}
+                ${notes}
               </div>
             </div>`
             );
@@ -40,11 +48,22 @@ function reloadSentence(){//this is the same function name as app.js to reload t
             choice = "N/A";
         else
             choice = listOfResponses[i+1]=="false" ? "incorrect" : "correct";
-        $(`#sentence${i}`).text(sentences[localStorage.getItem("Simplified")=="false" ? "Traditional" : "Simplified"][DICTIONARY][Math.floor(listOfResponses[i]/2)][rightorwrong]);
+        $(`#sentence${i}`).text(sentences[simpOrTrad()][DICTIONARY][Math.floor(listOfResponses[i]/2)][rightorwrong]);
     }
 }
 
 function restart(){
     localStorage.removeItem(DICTIONARY);
     window.location.href = `main.html?dict=${DICTIONARY}`;
+}
+
+function getNotes(specialnumber){
+    let notes = "<br>";
+    Object.keys(sentences[simpOrTrad()][DICTIONARY]["Notes"]).forEach(function(item){
+        if (sentences[simpOrTrad()][DICTIONARY]["Notes"][item][1].includes(Math.floor(specialnumber/2))){
+            notes+=(sentences[simpOrTrad()][DICTIONARY]["Notes"][item][0] + "\n");
+        }
+    });
+    console.log(notes);
+    return notes;
 }
