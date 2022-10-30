b9917514086312728cf5ecc23635ebf259736453
anthonypoon12 <anthony@poons.us>
2022-10-30 13:55:32 -0400

    fixed accordion, fixed trad simp

--- feedback.js
+++ feedback.js
@@ -7,6 +7,7 @@ if (localStorage.getItem(DICTIONARY)==null){
 }
 var listOfResponses = localStorage.getItem(DICTIONARY).split(",");
 populateBodies();
+
 function populateBodies(){
     for (let i = 0; i < listOfResponses.length;i=i+2){
         let rightorwrong = listOfResponses[i]%2==0? "wrong" : "right";
@@ -17,22 +18,32 @@ function populateBodies(){
             choice = listOfResponses[i+1]=="false" ? "incorrect" : "correct";
         $("#accordion").append(
             `<div class="accordion-item">
-            <h2 class="accordion-header" id="headingOne">
-              <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-              ${sentences[$(".btn-convert").text()][DICTIONARY][Math.floor(listOfResponses[i]/2)][rightorwrong]}
+            <h2 class="accordion-header" id="heading${i}">
+              <button class="accordion-button fs-2 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${i}" aria-expanded="true" aria-controls="collapse${i}">
+              <span id="sentence${i}">${sentences[localStorage.getItem("Simplified")=="false" ? "Traditional" : "Simplified"][DICTIONARY][Math.floor(listOfResponses[i]/2)][rightorwrong]}</span>
               </button>
             </h2>
-            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+            <div id="collapse${i}" class="accordion-collapse collapse" aria-labelledby="heading${i}" data-bs-parent="#accordion">
               <div class="accordion-body fs-5">
-                You chose ${listOfResponses[i+1]}
+                You chose ${listOfResponses[i+1]}.
               </div>
             </div>`
             );
     }
 }
+
 function reloadSentence(){//this is the same function name as app.js to reload that specific sentence, simpTrad uses this function to switch scripts
-    location.reload();
+    for (let i = 0; i < listOfResponses.length;i=i+2){
+        let rightorwrong = listOfResponses[i]%2==0? "wrong" : "right";
+        let choice;
+        if (listOfResponses[i+1]=="N/A")
+            choice = "N/A";
+        else
+            choice = listOfResponses[i+1]=="false" ? "incorrect" : "correct";
+        $(`#sentence${i}`).text(sentences[localStorage.getItem("Simplified")=="false" ? "Traditional" : "Simplified"][DICTIONARY][Math.floor(listOfResponses[i]/2)][rightorwrong]);
+    }
 }
+
 function restart(){
     localStorage.removeItem(DICTIONARY);
     window.location.href = `main.html?dict=${DICTIONARY}`;
